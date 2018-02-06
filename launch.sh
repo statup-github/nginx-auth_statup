8357ad30551c5a175faec5067fbf6f51f19821d1
Stefan Fritsch <stefan.fritsch@stat-up.com>
2018-02-06 17:45:42 +0100

    working

---
+++ launch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm /etc/nginx/conf.d/default.conf || :
+# envsubst < auth.conf > /etc/nginx/conf.d/auth.conf
+envsubst '${FORWARD_HOST},${FORWARD_PORT}' < auth.conf > /etc/nginx/conf.d/auth.conf
+
+envsubst < auth.htpasswd > /etc/nginx/auth.htpasswd
+
+nginx -g "daemon off;"
